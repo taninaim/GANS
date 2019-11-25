01d6e1e04e5fb741c2cf111cbcc7e5fa7750b265
taninaim <taninaim@gmail.com>
2019-11-26 00:02:26 +0700

    Add CGAN

--- CGAN/main.py
+++ CGAN/main.py
@@ -129,3 +129,65 @@ cgan = build_cgan(generator, discriminator)
 
 cgan.compile(loss='binary_crossentropy', optimizer=Adam())
 
+accuracies = []
+losses = []
+
+def train(iterations, batch_size, sample_interval):
+    (X_train, y_train), (_, _) = mnist.load_data()
+
+    X_train = X_train / 127.5 - 1.
+    X_train = np.expand_dims(X_train, axis=3)
+    
+    real = np.ones((batch_size, 1))
+    fake = np.zeros((batch_size, 1))
+
+    for iteration in range(iterations):
+        idx = np.random.randint(0, X_train.shape[0], batch_size)
+        imgs, labels = X_train[idx], y_train[idx]
+
+        z = np.random.normal(0, 1, (batch_size, z_dim))
+        gen_imgs = generator.predict([z, labels])
+
+        d_loss_real = discriminator.train_on_batch([imgs, labels], real)
+        d_loss_fake = discriminator.train_on_batch([gen_imgs, labels], fake)
+        d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
+
+        z = np.random.normal(0, 1, (batch_size, z_dim))
+
+        labels = np.random.randint(0, num_classes, batch_size).reshape(-1, 1)
+
+        g_loss = cgan.train_on_batch([z, labels], real)
+
+        if (iteration + 1) % sample_interval == 0:
+            print("%d [D loss: %f, acc.: %.2f%%] [G loss: %f]" % (iteration + 1, d_loss[0], 100*d_loss[1], g_loss))
+            losses.append((d_loss[0], g_loss))
+            accuracies.append(100 * d_loss[1])
+
+            sample_images()
+
+def sample_images(image_grid_rows=2, image_grid_columns=5):
+    z = np.random.normal(0, 1, (image_grid_rows * image_grid_columns, z_dim))
+    labels = np.arange(0, 10).reshape(-1, 1)
+    gen_imgs = generator.predict([z, labels])
+    gen_imgs = 0.5 * gen_imgs + 0.5
+    fig, axs = plt.subplots(
+        image_grid_rows,
+        image_grid_columns,
+        figsize=(10, 4),
+        sharey=True,
+        sharex=True
+    )
+
+    cnt = 0
+    for i in range(image_grid_rows):
+        for j in range(image_grid_columns):
+            axs[i, j].imshow(gen_imgs[cnt, :, :, 0], cmap='gray')
+            axs[i, j].axis('off')
+            axs[i, j].set_title('Digit: %d' % labels[cnt])
+            cnt += 1
+
+iterations = 12000
+batch_size = 32
+sample_interval = 10
+
+train(iterations, batch_size, sample_interval)
